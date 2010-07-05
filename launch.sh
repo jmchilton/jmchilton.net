230ad0adf2623f3cec3ca021d53fea477f8fc75e
john <john@eeetouch.(none)>
2010-07-05 17:24:46 -0500

    Updating launch.sh for Java 1.5 and limitted memory

--- launch.sh
+++ launch.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-java -cp "src:lib/*" clojure.main src/net/jmchilton/www/launch.clj
+THE_CLASSPATH=
+for i in `ls ./lib/*.jar`
+do
+  THE_CLASSPATH=${THE_CLASSPATH}:${i}
+done
+
+java -Xmx248m -cp "src:${THE_CLASSPATH}" clojure.main src/net/jmchilton/www/launch.clj
