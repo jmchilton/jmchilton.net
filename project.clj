fd04df7f1ce83a023ed179ae2b65192c9f7e45d5
John Chilton <jmchilton@gmail.com>
2012-12-08 11:06:08 -0600

    'Update' clojure dependencies for release of 1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject www "0.0.1-SNAPSHOT"
   :description "Test WWW Project"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 ;                 [clojure-http-client/clojure-http-client "1.1.0-SNAPSHOT"]
                  [ring/ring "0.2.5"]
                  [ehcache/ehcache "1.2.3"]
